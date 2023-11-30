a2bc0fca2d1e61020ca4b7f4e28520092681d103
Yisreal <huyucong34@gmail.com>
2023-11-30 22:48:11 +0800

    build site

---
+++ docs/assets/js/data/swcache.js
@@ -0,0 +1,57 @@
+const resource = [
+  /* --- CSS --- */
+  '/deep_learning/assets/css/jekyll-theme-chirpy.css',
+
+  /* --- PWA --- */
+  '/deep_learning/app.js',
+  '/deep_learning/sw.js',
+
+  /* --- HTML --- */
+  '/deep_learning/index.html',
+  '/deep_learning/404.html',
+
+  
+    '/deep_learning/categories/',
+  
+    '/deep_learning/tags/',
+  
+    '/deep_learning/archives/',
+  
+    '/deep_learning/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/deep_learning/assets/img/favicons/android-chrome-192x192.png',
+    '/deep_learning/assets/img/favicons/android-chrome-512x512.png',
+    '/deep_learning/assets/img/favicons/apple-touch-icon.png',
+    '/deep_learning/assets/img/favicons/favicon-16x16.png',
+    '/deep_learning/assets/img/favicons/favicon-32x32.png',
+    '/deep_learning/assets/img/favicons/favicon.ico',
+    '/deep_learning/assets/img/favicons/mstile-150x150.png',
+    '/deep_learning/assets/js/dist/categories.min.js',
+    '/deep_learning/assets/js/dist/commons.min.js',
+    '/deep_learning/assets/js/dist/home.min.js',
+    '/deep_learning/assets/js/dist/misc.min.js',
+    '/deep_learning/assets/js/dist/page.min.js',
+    '/deep_learning/assets/js/dist/post.min.js'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'hhh-hyc.github.io',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [];
+
